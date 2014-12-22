ea07e7739244c3611b9ea12cc18a3d2ee8ff5b0c
Lindsay Hammon <lindsay.work.mail@gmail.com>
2014-12-22 14:04:06 -0700

    generated the static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'home/about'
+
+  get 'home/contact'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
